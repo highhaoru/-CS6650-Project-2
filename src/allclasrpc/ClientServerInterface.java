afb74fc8ce005dbe7d94d620cf39c2e4a9510544
Hao Jin <jin.ha@northeastern.edu>
2023-03-05 13:11:21 -0500

    first commit

--- src/allclasrpc/ClientServerInterface.java
+++ src/allclasrpc/ClientServerInterface.java
@@ -1,2 +1,13 @@
-package allclasrpc;public class ClientServerInterface {
+package allclasrpc;
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientServerInterface extends Remote{
+
+    public void processPutRequest (String key, String value) throws RemoteException;
+    public String processGetRequest(String key) throws RemoteException;
+    public void processDeleteRequest(String key) throws RemoteException;
+
 }
--- src/allclasrpc/RmiRegister.java
+++ src/allclasrpc/RmiRegister.java
@@ -1,2 +1,33 @@
-package allclasrpc;public class RmiRegister {
+package allclasrpc;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class RmiRegister {
+    private static final Logger LOGGER = LogManager.getLogger(RmiRegister.class);
+
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            LOGGER.error(" java RmiRegister <Port Number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.valueOf(args[0]).intValue();
+
+        try{
+            ClientServerInterface messageServerObject = new ClientServerInterfaceImpl();
+            Registry registry = LocateRegistry.createRegistry(portNumber);
+            registry.bind("ClientServerInterfaceImpl", messageServerObject);
+            LOGGER.info("Object binding is done");
+
+        }catch(Exception ex){
+
+        }
+
+    }
+
 }
